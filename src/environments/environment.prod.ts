d75424d0a8b836842afd59139e21b191fab6249f
felipesantos22 <felipevs2018@gmail.com>
2023-12-02 22:39:55 -0300

    feat: inserindo api da url

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://agency-tour-backend-a10abe72bb88.herokuapp.com/api',
-  };
-  
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://agency-tour-backend-a10abe72bb88.herokuapp.com/api',
-};
