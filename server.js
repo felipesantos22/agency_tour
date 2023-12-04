fbfab414f739db77490fca9ce45a8a7e916a744f
felipesantos22 <felipevs2018@gmail.com>
2023-12-04 16:02:23 -0300

    feat: change json

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-// const express = require("express");
-// const path = require("path");
-
-// const app = express();
-
-// // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-// app.use(express.static(__dirname + "/dist/agency-tour"));
-
-// app.get("/*", function (req, res) {
-//   res.sendFile(path.join(__dirname + "/dist/agency-tour/index.html"));
-// });
-
-// // Inicia a aplicação pela porta configurada
-// app.listen(process.env.PORT || 8080);
-
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/app-heroku'));
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + "/dist/agency-tour"));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/app-heroku/'}),
-);
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/agency-tour/index.html"));
+});
 
-// Start the app by listening on the default Heroku port
+// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
+
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static('./dist/app-heroku'));
+
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/app-heroku/'}),
+// );
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
