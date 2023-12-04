5e0d10dd5d6206c79fd33516d0aef9a2d2d521b0
felipesantos22 <felipevs2018@gmail.com>
2023-12-04 15:56:09 -0300

    feat: change json

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
-const express = require("express");
-const path = require("path");
+// const express = require("express");
+// const path = require("path");
+
+// const app = express();
+
+// // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+// app.use(express.static(__dirname + "/dist/agency-tour"));
+
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname + "/dist/agency-tour/index.html"));
+// });
+
+// // Inicia a aplicação pela porta configurada
+// app.listen(process.env.PORT || 8080);
+
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + "/dist/agency-tour"));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/app-heroku'));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/agency-tour/index.html"));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/app-heroku/'}),
+);
 
-// Inicia a aplicação pela porta configurada
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
