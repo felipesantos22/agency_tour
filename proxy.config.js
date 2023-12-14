8956a7e50951d1dcbe544c267d47a7f8d907c6fc
felipesantos22 <felipevs2018@gmail.com>
2023-12-13 22:52:02 -0300

    alterando url

--- proxy.config.js
+++ proxy.config.js
@@ -1,11 +1,11 @@
 const proxy = [
   {
-    context: ["/api/*"],
+    context: ["/api/country"],
     target: "https://agency-tour-backend-a10abe72bb88.herokuapp.com",
     secure: true,
     logLevel: "debug",
     changeOrigin: true,
-    pathRewrite: { "^/api": "" }, // Isso removerá "/api" do caminho
+    pathRewrite: { "^/api": "" },
   },
 ];
 
--- src/app/services/packages.service.ts
+++ src/app/services/packages.service.ts
@@ -1,21 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, catchError, retry } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Country } from '../models/Country';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PackagesService {
-  private url: string = '/api';
+  private url: string = 'https://agency-tour-backend-a10abe72bb88.herokuapp.com/api/country';
 
   constructor(private http: HttpClient) {}
 
-  httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-  };
-
   index(): Observable<Country[]> {
-    return this.http.get<Country[]>(this.url).pipe(retry(2));
+    return this.http.get<Country[]>(this.url);
   }
 }
